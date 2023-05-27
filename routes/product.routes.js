1ad7b024631bdab1b98b72ab58605f20aaf178d6
hambalee <hambalee@outlook.com>
2023-05-27 16:48:31 +0700

    finish

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,13 +1,23 @@
 const express = require('express')
 const router = express.Router()
 const path = require('path')
-const indexPage = path.join(__dirname, "../templates/index.html")
+const indexPage = path.join(__dirname, "../templates/home.html")
+const notFoundPage = path.join(__dirname, "../templates/404.html")
 
-router.get("/", (req, res) => {
+function homepage(req, res) {
     res.status(200)
     res.type('html')
     res.sendFile(indexPage)
-})
+}
+
+function notFound(req, res) {
+    res.status(404)
+    res.type('html')
+    res.sendFile(notFoundPage)
+}
+
+router.get("/", homepage)
+router.get("/home", homepage)
 
 router.get("/products", (req, res) => {
     res.send("Hello product")
@@ -15,9 +25,12 @@ router.get("/products", (req, res) => {
 
 router.get("/product/:id", (req, res) => {
     const productId = req.params.id
-    if(productId == 1) {
-        res.sendFile(path.join(__dirname, "../templates/product1.html"))
+    if (productId) {
+        // res.sendFile(path.join(__dirname, `../templates/product${productId}.html`))
+        res.sendFile(path.join(__dirname, `../templates/${productId}`))
     }
 })
+
+router.get("*", notFound)
 
 module.exports = router
