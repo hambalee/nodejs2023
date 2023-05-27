cfcfff22588ad1aaa688de916874e0e37d9e6a2e
hambalee <hambalee@outlook.com>
2023-05-27 12:13:24 +0700

    add home and 404 page not found response

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const http = require('http')
 
 const server = http.createServer(function (req, res) {
-    res.write("<h1>Hello</h1>")
-    res.end()
+    const path = req.url
+    console.log("path is " + path);
+    if (path === '/home') {
+        res.write("<h1>Hello Home</h1>")
+        res.end()
+    } else {
+        res.statusCode = 200;
+        res.end("<h1>404 page not found</h1>")
+    }
 })
 
 server.listen(3001, () => {
