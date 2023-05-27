a8a5cd920f327a6876f2000aa57c1206da5d3640
hambalee <hambalee@outlook.com>
2023-05-27 10:58:23 +0700

    callback synchronous

--- callback.js
+++ callback.js
@@ -1,14 +1,33 @@
+const fileName1 = "lisa.jpg"
+const fileName2 = "janny.jpg"
+const fileName3 = "ink.jpg"
+
 //Download file 3s => display file name
-function download(callback) {
-    console.log('downloading...');
-    const filename = 'document.docx'
+function download(fileName, callback) {
+    console.log(fileName + ' is downloading...');
     setTimeout(() => {
-        callback(filename)
+        callback(fileName)
     }, 3000);
 }
 
-function displayFileName(result) {
-    console.log('filename: ' + result);
+function display(result) {
+    console.log(result + ' is downloaded');
 }
 
-download(displayFileName)
+download(fileName1, () => {
+    display(fileName1)
+    download(fileName2, () => {
+        display(fileName2)
+        download(fileName3, () => {
+            display(fileName3)
+        })
+    })
+})
+
+// result
+// lisa.jpg is downloading...
+// lisa.jpg is downloaded
+// janny.jpg is downloading...
+// janny.jpg is downloaded
+// ink.jpg is downloading...
+// ink.jpg is downloaded
